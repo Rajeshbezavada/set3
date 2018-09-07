e47dca51da49df7605690c252e467d1e05145eb5
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-09-07 02:06:49 -0400

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Time
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		int hours=t/60;
+		int minute=t%60;
+		System.out.println(hours+" "+minute);
+	
+		}
+} 
